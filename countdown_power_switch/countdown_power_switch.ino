60eae3ca6f59271410c00855b313b0560ca406fd
Arjen Gerstel <mail@arjengerstel.nl>
2015-09-08 23:01:41 +0200

    Added comment to countdown timer firmware

--- countdown_power_switch/countdown_power_switch.ino
+++ countdown_power_switch/countdown_power_switch.ino
@@ -1,17 +1,46 @@
+/*
+ * Smart power countdown timer   (c) 2015 Arjen Gerstel
+ * 
+ * Code for a device that can temporarily switch on power to any mains-powered device.
+ * Features:
+ * One-button press to activate
+ * Button to reset the timer
+ * Button for immediate shutdown
+ * LED that blinks when timer has nearly expired
+ * Zero power use when switched off
+ * 
+ */
+
+
+
+// Pin number of control LED
 #define LED_PIN 0
+
+// Pin number of relay that cuts power when done
 #define POWER_RELAY 1
+
+// Pin number of Countdown Reset switch
 #define COUNTDOWN_RESET 2
+
+// Pin number of Off switch
 #define OFF_BUTTON 3
 
+// Duration of power in seconds
 #define COUNTDOWN_TIME 10
+
+// Length of warning blink before cutting power
 #define WARNING_BLINK 3
 
+// Interval for loop
 #define LOOP_DELAY 500
 
 #define FALSE 0
 #define TRUE 1
 
+// Global variable for Time to cut power 
 unsigned long EndTime; 
+
+// Global variable for Time to start blinking (before cutting power)
 unsigned long WarningTime; 
 boolean LedOn = FALSE; 
 
@@ -25,7 +54,7 @@ void setup() {
 
 // initialize ports
   digitalWrite(LED_PIN, HIGH);
-  digitalWrite(POWER_RELAY, HIGH);   // set high to cut primary power to circuit
+  digitalWrite(POWER_RELAY, LOW);   // set high to cut primary power to circuit
 
   ResetTimers();
 }
